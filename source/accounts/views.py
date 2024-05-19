93fdb18eb3b9d769154a6bfb418c4728160ef578
GLeb Tarakanov <glebtgd@gmail.com>
2024-05-19 16:26:29 +0500

    LMS-11 Реализован функционал пользователя и личного профиля
    1) Реализован личный профиль
    2) Реализовано редактирование своих личных данных
    3) Реализована смена email
    4) Реализована смена пароля
    5) Реализован сброс пароля

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,18 +1,19 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import PasswordChangeView
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.views.generic import CreateView, TemplateView, DetailView, UpdateView
+from django.views.generic import CreateView, TemplateView, DetailView, UpdateView, FormView
 
-from accounts.forms import CustomUserCreationForm, LoginForm, UserForm
+from accounts.forms import (
+    CustomUserCreationForm, LoginForm, UserForm, EmailChangeForm, CustomPasswordChangeForm,
+)
 
-
-# Create your views here.
 
 class LoginView(TemplateView):
     template_name = 'login.html'
@@ -36,7 +37,7 @@ class LoginView(TemplateView):
         login(request, user)
         if next:
             return redirect(next)
-        return redirect('index')
+        return redirect('user_detail', pk=user.pk)
 
 
 class RegisterView(CreateView):
@@ -86,21 +87,65 @@ def logout_view(request):
     logout(request)
     return redirect('index')
 
-# class UserDetailView(DetailView):
-#     template_name = 'user_detail.html'
-#     model = get_user_model()
-#     context_object_name = 'user_obj'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['favorite_photos'] = self.request.user.user_favorites.all()
-#         return context
-#
-#
-# class UserUpdateView(UpdateView):
-#     template_name = 'user_update.html'
-#     model = get_user_model()
-#     form_class = UserForm
-#
-#     def get_success_url(self):
-#         return reverse('user_detail', kwargs={'pk': self.object.pk})
+
+class UserDetailView(DetailView):
+    template_name = 'user_detail.html'
+    model = get_user_model()
+    context_object_name = 'user_obj'
+
+
+class UserUpdateView(UpdateView):
+    template_name = 'user_update.html'
+    model = get_user_model()
+    form_class = UserForm
+
+    def get_success_url(self):
+        return reverse('user_detail', kwargs={'pk': self.object.pk})
+
+
+class UserEmailChangeView(FormView):
+    template_name = 'email_change.html'
+    form_class = EmailChangeForm
+
+    def form_valid(self, form):
+        user = self.request.user
+        new_email = form.cleaned_data['new_email']
+        self.send_confirmation_email(user, new_email)
+        return redirect('user_detail', pk=user.pk)
+
+    def send_confirmation_email(self, user, new_email):
+        current_site = get_current_site(self.request)
+        mail_subject = 'Confirm your new email address.'
+        message = render_to_string('email_change_confirm.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'new_email': urlsafe_base64_encode(force_bytes(new_email)),
+            'token': default_token_generator.make_token(user),
+        })
+        send_mail(mail_subject, message, 'your_email@example.com', [new_email])
+
+
+def confirm_email_change(request, uidb64, token, new_email_encoded):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        new_email = force_str(urlsafe_base64_decode(new_email_encoded))
+        user = get_user_model().objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.email = new_email
+        user.save()
+        return redirect('user_detail', pk=user.pk)
+    else:
+        return render(request, 'activation_invalid.html')
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    form_class = CustomPasswordChangeForm
+    template_name = 'password_change.html'
+    success_url = reverse_lazy('user_detail')
+
+    def get_success_url(self):
+        return reverse_lazy('user_detail', kwargs={'pk': self.request.user.pk})
