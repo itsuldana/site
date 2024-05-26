950d81d324f280a011aee7eb7fa1b1f27cb7dd0c
GLeb Tarakanov <glebtgd@gmail.com>
2024-05-27 02:31:49 +0500

    LMS-12 Настроена мультиязычность на двух языках: Русский и Английский

--- source/core/settings.py
+++ source/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,14 +113,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGES = [
+    ('en', _('English')),
+    ('ru', _('Russian')),
+]
+
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
+# Включение интернационализации
 USE_I18N = True
+# Включение локализации
+USE_L10N = True
 
 USE_TZ = True
 
+# Пути для файлов перевода
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
